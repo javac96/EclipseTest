4297ca2a5aa366df088817c550efac9a7393f1e1
Hasan <rhasan3860@yahoo.com>
2017-07-11 23:54:42 -0400

    Comitted

---
+++ GoogleTest/src/com/google/Login/Login.java
@@ -0,0 +1,25 @@
+package com.google.Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("www.gmail.com");
+		
+		driver.findElement(By.xpath("//*[@id='identifierId']")).sendKeys("rhasan3860@gmail.com");
+
+		driver.findElement(By.id("password")).sendKeys("hflkfkl");
+		driver.findElement(By.xpath("//*[@id='passwordNext']/content/span"));
+		System.out.println("Done finally great");
+		
+
+	}
+
+}
